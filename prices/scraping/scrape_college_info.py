10e9a2dafd06c75be05255cede8b22ba3cd80f53
mattcoley <mattcoley@optonline.net>
2015-11-09 19:12:59 +0000

    Basis for scraping student data. Will connect to model soon.

---
+++ prices/scraping/scrape_college_info.py
@@ -0,0 +1,42 @@
+from lxml import html
+import requests
+import urllib2
+import sys
+
+COLLEGE_LIST = ["Christ's College", "Churchill College", "Clare College"]
+BASE_URL = 'http://www.undergraduate.study.cam.ac.uk/colleges/'
+
+
+def run_scraper():
+    # Remove unwanted punctuation, spaces, and capitalization (Christ's College -> christs-college)
+    colleges = map(lambda val: val.replace("'", ''), COLLEGE_LIST)
+    colleges = map(lambda val : val.replace(' ', '-'), colleges)
+    colleges = map(lambda val: val.lower(), colleges)
+
+    college_data = {college_name: find_college_info(college_name) for college_name in colleges}
+    print college_data
+
+def find_college_info(college_name):
+    college_url = BASE_URL + college_name
+    request = urllib2.urlopen(college_url)
+    html_page = request.read()
+
+    tree = html.fromstring(html_page)
+
+    base_path = '//*[@id="block-views-college-views-block-3"]/div/div/div/div/div/div[5]/div[2]/div[{}]/text()'
+    return {'num_undergrad': label_to_num(tree.xpath(base_path.format(1))),
+            'num_year': label_to_num(tree.xpath(base_path.format(2))),
+            'num_grad': label_to_num(tree.xpath(base_path.format(3)))}
+
+
+
+def label_to_num(label):
+    return int(label[0].split(' ')[0].replace('c', '')) or 0
+
+def main():
+    run_scraper()
+
+if __name__ == '__main__':
+    main()
+
+
